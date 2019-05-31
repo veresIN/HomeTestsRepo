d9d725d123b2ebde129d13eaf296b11998cd4ee3
i.veres <ivanveresr@gmail.com>
2019-05-31 20:10:50 +0300

    MyQuestions class

--- Hello.py
+++ Hello.py
@@ -1,3 +1,6 @@
+import MyQuestions
+
 print("Hi")
 u_name = input("Please Enter Yor Name")
 print("Hello closer " + u_name)
+MyQuestions.test()
---
+++ MyQuestions.py
@@ -0,0 +1,2 @@
+def test():
+    print ("call from mYq")
